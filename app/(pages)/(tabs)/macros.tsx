4c482dfa2231316d82c250a3ff7ca1a1f1a0efe2
Caleb Pace <pacecaleb2004+Git@gmail.com>
2025-04-11 13:29:22 +1200

    style: add basic styling for page layout

--- app/(pages)/(tabs)/macros.tsx
+++ app/(pages)/(tabs)/macros.tsx
@@ -1,11 +1,35 @@
-import {View, Text} from 'react-native'
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import {Link} from "expo-router";
 
 const Macros = () => {
     return (
-        <View>
-            <Text>Macros</Text>
+        <View style={styles.background}>
+            <View style={[styles.container, { height: 80, width: '100%' }]}>
+                <Link href="/(pages)/(tabs)/dashboard" style={styles.text}>{'<'}- (Back)</Link>
+            </View>
+            <Text style={[styles.text, { margin: 50, fontSize: 30 }]}>Macros</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#19181B',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    container: {
+        backgroundColor: '#2D2E31',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+    text: {
+        color: '#FFEEE5',
+        fontSize: 20,
+        margin: 20,
+    },
+});
+
 export default Macros
