aad2daef89a9c3fd6bf500853725604bb92f7c42
Josh Padua <jidpadua@gmail.com>
2025-04-30 11:00:31 +1200

    feat: add basic taskbar

--- app/(pages)/(tabs)/index.tsx
+++ app/(pages)/(tabs)/index.tsx
@@ -1,14 +1,9 @@
 import {Text, View} from "react-native";
 import {Link} from "expo-router";
 
-export default function Dashboard() {
+export default function Index() {
     return (
-        <View className="flex flex-col items-center">
-            <Text>Test</Text>
-            <Link href="/macros">macros</Link>
-            <Link href="/macros">profile</Link>
-            <Link href="/macros">settings</Link>
-            <Link href="/macros">workout</Link>
+        <View>
         </View>
     )
 }
---
+++ app/(pages)/(tabs)/macros.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Macros = () => {
+    return (
+        <View>
+            <Text>Macros</Text>
+        </View>
+    )
+}
+export default Macros
