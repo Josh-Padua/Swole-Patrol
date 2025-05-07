33f2cd0ce7aa742a6717b54883f0b3fa3a5b3c35
Nyima Tsering <twv3047@autuni.ac.nz>
2025-05-07 13:11:04 +1200

    Chore: base

--- app/(pages)/(tabs)/workouts.tsx
+++ app/(pages)/(tabs)/workouts.tsx
@@ -1,10 +1,14 @@
-import {View, Text, SafeAreaView} from 'react-native'
+import {View, Text, SafeAreaView, Button} from 'react-native'
 import React from 'react'
+import {useAuth} from "@/app/(auth)/AuthProvider";
+
 
 const Workouts = () => {
+    const { user, signOut, userData} = useAuth();
+    const day = new Date().toLocaleDateString('en-US', { weekday: 'long' });
     return (
-        <SafeAreaView className="bg-primary-background h-full">
-            <Text>Workouts</Text>
+        <SafeAreaView className="bg-primasry-background h-full">
+            <Text>{day}</Text>
         </SafeAreaView>
     )
 }
